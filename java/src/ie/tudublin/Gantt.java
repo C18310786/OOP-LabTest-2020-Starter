27f133205add30fc380bc0a302d96d707f16cc55
Roberto <robdmc34@gmail.com>
2020-04-22 15:30:08 +0100

    Alter start day working, end day not working

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -11,6 +11,8 @@ import processing.data.TableRow;
 public class Gantt extends PApplet
 {	
 	ArrayList<Tasks> task = new ArrayList<Tasks>();
+	public int lSelect = -1;
+	public int rSelect = -1;
 
 	public void settings()
 	{
@@ -68,16 +70,68 @@ public class Gantt extends PApplet
 	
 	public void mousePressed()
 	{
-		println("Mouse pressed");	
+		println("Mouse pressed");
+
+		float border = width * 0.10f;
+		float leftborder = 200;
+		float rectHeight = 35;
+		float allowance = 10;
+
+		for(int i = 0; i < task.size() ; i++)
+		{
+			Tasks tsk = task.get(i);
+
+			float start = map(tsk.getStart(), 2, 32, leftborder , width - border);
+			float end = map(tsk.getEnd(), 2, 32, leftborder , width - border);
+
+
+			if(mouseX < start + allowance && mouseX > start - allowance)
+			{
+				lSelect = i;
+				rSelect = -1;
+			}
+			if(mouseX < end + allowance && mouseX > end - allowance)
+			{
+				rSelect = i;
+				lSelect = -1;
+			}			
+		}
 	}
 
 	public void mouseDragged()
 	{
 		println("Mouse dragged");
+		int num;
+		int totalDays = 30;
+
+		if(lSelect > -1)
+		{
+			Tasks tsk = task.get(lSelect);
+			println(task.get(lSelect));
+			num = (int) map(mouseX, 0, width, 0, totalDays);
+			if(tsk.getEnd() - num >= 1 && num > 0 && num < tsk.getEnd())
+			{
+				tsk.setStart(num);
+			}
+		}
+		
+		if(rSelect > -1)
+		{
+			Tasks tsk = task.get(rSelect);
+			num = (int) map(mouseX, 0, width, 0, totalDays);
+			if(tsk.getStart() - num >= 1 && num > tsk.getStart() && num <= totalDays)
+			{
+				tsk.setEnd(num);
+			}
+		}
 	}
 
-	
-	
+	public void mouseReleased()
+	{
+		lSelect = -1;
+		rSelect = -1;
+	}
+
 	public void setup() 
 	{
 		loadTasks();
